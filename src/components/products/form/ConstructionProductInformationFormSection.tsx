6fcef23d707886830524dcbeafcd6c8b205711bb
Lucas Johnson <alohavocado@gmail.com>
2025-06-15 14:20:34 +0000

    "master"

---
+++ src/components/products/form/ConstructionProductInformationFormSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface MyComponentProps {
+  // Define your props here
+}
+
+const MyComponent: React.FC<MyComponentProps> = (props) => {
+  return (
+    <div>
+      {/* Your component JSX goes here */}
+    </div>
+  );
+};
+
+export default MyComponent;
